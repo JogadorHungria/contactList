0a6d74e85ea5ccdce9500569df6f107fead64183
joilson <joilsonhungrianv@gmail.com>
2023-07-28 09:32:47 -0300

    feat: adding structure form html

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email">email</label>
+          <input id="email" type="text" />
+          <label htmlFor="password">password</label>
+          <input id="password" type="text" />
+        </form>
+      </div>
+    </main>
+  );
 }
